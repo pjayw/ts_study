891e7e2aa72e877b046b7c5461fdfb9170d9aa42
박지원 <tlsxp13@naver.com>
2024-08-25 23:46:46 +0900

    2024.8.25 학습

---
+++ onebite_typescript/section3/src/chapter6.ts
@@ -0,0 +1,57 @@
+// 타입 단언
+
+type Person = {
+  name: string
+  age: number
+}
+
+let person = {} as Person // 직접 단언 type assertion
+person.name = "이정환"
+person.age = 27
+
+type Dog = {
+  name: string
+  color: string
+}
+
+let dog = {
+  name: "돌돌이",
+  color: "brown",
+  breed: "진도",
+} as Dog
+
+// 아무 상황에서 쓰면 안되여
+
+// 타입 단언의 규칙
+// 값 as 단언의 형태로 단언식 작성
+// 값의 타입이 A이고, 단언하는 타입이 B일 때
+// A가 B의 슈퍼타입이거나 서브타입이어야 함
+
+let num1 = 10 as never
+let num2 = 20 as unknown
+// let num3 = 10 as string => 얘는 슈퍼, 서브가 아니라 안됨
+let num3 = 10 as unknown as string // 위의 거를 실행시키려면 이렇게
+
+// const 단언
+
+let num4 = 10 as const // const 선언과 같은 효과
+
+let cat = {
+  name: "야옹이",
+  color: "yellow"
+} as const
+
+// Non Null 단언
+
+type Post = {
+  title: string
+  author?: string
+}
+
+let post: Post = {
+  title: "게시글1",
+  author: "이정환",
+}
+
+const len: number = post.author!.length
+// !만 붙여주면 non, null이 아니라고 생각함
